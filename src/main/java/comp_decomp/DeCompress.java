f1a2f25f2e8b9686f18d521f9d8642701ba4fc35
RaghuCodeWizard <137168147+RaghuCodeWizard@users.noreply.github.com>
2023-06-24 18:37:37 +0530

    DeCompress.java

--- src/main/java/comp_decomp/DeCompress.java
+++ src/main/java/comp_decomp/DeCompress.java
@@ -1,34 +1,35 @@
-package comp_decomp;
+package Compress_DeCompress;
 
-import javax.swing.*;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 public class DeCompress {
-    public static void method(File file) throws IOException{
+    public static void method(File file) throws IOException {
+        // Get the directory path of the file
         String fileDirectory = file.getParent();
+
+        // Create FileInputStream to read the compressed file
         FileInputStream fis = new FileInputStream(file);
+
+        // Create GZIPInputStream to decompress the file
         GZIPInputStream gzip = new GZIPInputStream(fis);
-        FileOutputStream fos = new FileOutputStream(fileDirectory+"/DeCompressedFile.gz");
+
+        // Create FileOutputStream to write the decompressed file
+        FileOutputStream fos = new FileOutputStream(fileDirectory + "/DeCompressedFile.gz");
+
+        // Create a buffer for reading compressed data
         byte[] buffer = new byte[1024];
         int len;
-        while((len= gzip.read(buffer))!=-1){
-            fos.write(buffer,0,len);
+
+        // Read data from the GZIPInputStream and write it to the FileOutputStream
+        while ((len = gzip.read(buffer)) != -1) {
+            fos.write(buffer, 0, len);
         }
+
+        // Close the GZIPInputStream, FileOutputStream, and FileInputStream
         gzip.close();
         fos.close();
         fis.close();
     }
-
-    public static void main(String[] args) throws IOException {
-        JFileChooser fileChooser = new JFileChooser();
-        int response = fileChooser.showOpenDialog(null);
-        if (response == JFileChooser.APPROVE_OPTION) {
-            File selectedFile = fileChooser.getSelectedFile();
-            method(selectedFile);
-    }
-}
 }
