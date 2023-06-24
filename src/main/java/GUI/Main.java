04d82c10ed11b75e8c783dd2c0c4361d8132e780
RaghuCodeWizard <137168147+RaghuCodeWizard@users.noreply.github.com>
2023-06-24 18:36:13 +0530

    Main.java

--- src/main/java/GUI/Main.java
+++ src/main/java/GUI/Main.java
@@ -1,8 +1,10 @@
-package GUI;
+package org.example;
 
+import GUI.AppFrame;
 
 public class Main {
     public static void main(String[] args) {
+        // Create an instance of the AppFrame class to launch the GUI application
         AppFrame frame = new AppFrame();
     }
 }
