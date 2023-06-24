6bae3ea1c3cba09a40c2e2a4275cdc9b8cde759e
RaghuCodeWizard <137168147+RaghuCodeWizard@users.noreply.github.com>
2023-06-24 18:37:08 +0530

    Compress.java

--- src/main/java/comp_decomp/Compress.java
+++ src/main/java/comp_decomp/Compress.java
@@ -1,34 +1,34 @@
-package comp_decomp;
+package Compress_DeCompress;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.zip.GZIPOutputStream;
-import javax.swing.JFileChooser;
 
 public class Compress {
     public static void method(File file) throws IOException {
+        // Get the directory path of the file
         String fileDirectory = file.getParent();
+
+        // Create FileInputStream to read the file
         FileInputStream fis = new FileInputStream(file);
+
+        // Create FileOutputStream to write the compressed file
         FileOutputStream fos = new FileOutputStream(fileDirectory + "/CompressedFile.gz");
+
+        // Create GZIPOutputStream to compress the file
         GZIPOutputStream gzip = new GZIPOutputStream(fos);
+
+        // Create a buffer for reading file data
         byte[] buffer = new byte[1024];
         int len;
+
+        // Read data from the file and write it to the GZIPOutputStream
         while ((len = fis.read(buffer)) != -1) {
             gzip.write(buffer, 0, len);
         }
+
+        // Close the GZIPOutputStream, FileOutputStream, and FileInputStream
         gzip.close();
         fos.close();
         fis.close();
-    }
-
-    public static void main(String[] args) throws IOException {
-        JFileChooser fileChooser = new JFileChooser();
-        int response = fileChooser.showOpenDialog(null);
-        if (response == JFileChooser.APPROVE_OPTION) {
-            File selectedFile = fileChooser.getSelectedFile();
-            method(selectedFile);
-        }
     }
 }
